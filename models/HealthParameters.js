a57abfda6e1c2859e0dd6bfe9cc0b8b036ca7fff
Mohammad Athar <157023731+m-atharkhan@users.noreply.github.com>
2025-07-18 21:43:29 +0530

    Create HealthParameters.js

---
+++ models/HealthParameters.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const healthSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  spo2: Number,
+  ecg: String,
+  heartRate: Number,
+  bodyTemp: Number,
+  gsr: Number,
+  timestamp: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+export const HealthParameters = mongoose.models.HealthParameters || mongoose.model("HealthParameters", healthSchema);
