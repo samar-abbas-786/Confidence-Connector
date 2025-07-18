8590fa9bf32900fc75c3f78f0057b19ad49cd3dd
Mohammad Athar <157023731+m-atharkhan@users.noreply.github.com>
2025-07-18 21:39:29 +0530

    User Schema updated

--- models/User.js
+++ models/User.js
@@ -1,20 +1,31 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    role: {
-        type: String,
-        enum: ["doctor", "patient"],
-        required: true
-    }
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ["doctor", "patient"],
+    required: true
+  },
+  email: String,
+  phone: String,
+  age: Number,
+  gender: {
+    type: String,
+    enum: ["male", "female", "other"]
+  },
+  health: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "HealthParameters"
+  }
 });
 
 export const User = mongoose.models.User || mongoose.model("User", userSchema);
