758dc008866d7e7573607426d9daa3fa086ae05e
Samar Abbas <samarabbas172003@gmail.com>
2025-09-02 21:14:01 +0530

    add Home

--- components/Navbar/page.jsx
+++ components/Navbar/page.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
   }, []);
 
   const navLinks = [
-    // { name: "Home", href: "/" },
+    { name: "Home", href: "/" },
     { name: "About", href: "#about" },
     // { name: "Features", href: "#features" },
     // { name: "Team", href: "#team" },
